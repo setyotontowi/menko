fdade248c8ca19680cc1a577f924145422709374
setyotontowi <setyotontowi@gmail.com>
2020-09-06 20:15:25 +0700

    Add unit test

--- app/src/main/java/com/project/thisappistryingtomakeyoubetter/util/GeneralHelper.java
+++ app/src/main/java/com/project/thisappistryingtomakeyoubetter/util/GeneralHelper.java
@@ -1,7 +1,6 @@
 package com.project.thisappistryingtomakeyoubetter.util;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
