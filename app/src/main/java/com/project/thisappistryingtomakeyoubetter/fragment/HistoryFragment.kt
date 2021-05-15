467428f57f160b21c65a50feeda6092622d556d5
setyotontowi <setyotontowi@gmail.com>
2021-05-16 01:25:27 +0700

    Edit label in history #33

--- app/src/main/java/com/project/thisappistryingtomakeyoubetter/fragment/HistoryFragment.kt
+++ app/src/main/java/com/project/thisappistryingtomakeyoubetter/fragment/HistoryFragment.kt
@@ -2,6 +2,7 @@ package com.project.thisappistryingtomakeyoubetter.fragment
 
 import android.app.Dialog
 import android.content.DialogInterface
+import android.os.Build
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.widget.Toolbar
@@ -11,14 +12,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.thisappistryingtomakeyoubetter.App
 import com.project.thisappistryingtomakeyoubetter.R
+import com.project.thisappistryingtomakeyoubetter.adapter.ChipAdapter
 import com.project.thisappistryingtomakeyoubetter.adapter.TaskAdapter
 import com.project.thisappistryingtomakeyoubetter.databinding.DialogTaskBinding
 import com.project.thisappistryingtomakeyoubetter.databinding.FragmentHistoryBinding
+import com.project.thisappistryingtomakeyoubetter.model.Label
 import com.project.thisappistryingtomakeyoubetter.model.Task
 import com.project.thisappistryingtomakeyoubetter.model.TaskWithLabel
 import com.project.thisappistryingtomakeyoubetter.util.GeneralHelper
 import com.project.thisappistryingtomakeyoubetter.util.TaskViewModel
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 
 class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.ConfirmDialog {
@@ -27,6 +32,7 @@ class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.Conf
     private lateinit var toolbar: Toolbar
     private lateinit var taskAdapter: TaskAdapter
     private var tasks: MutableList<TaskWithLabel> = ArrayList()
+    private val labels: MutableList<Label> = ArrayList()
     @Inject
     lateinit var vmFactory: ViewModelProvider.Factory
     private val taskViewModel: TaskViewModel by viewModels { vmFactory}
@@ -53,6 +59,7 @@ class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.Conf
         taskViewModel.setTo(null)
         taskViewModel.apply {
             tasksWithLabel.observe(viewLifecycleOwner){handleListTask(it)}
+            label.observe(viewLifecycleOwner){handleLabel(it)}
         }
     }
 
@@ -72,7 +79,7 @@ class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.Conf
     }
 
     override fun onLongClick(task: TaskWithLabel) {
-        taskDialog(task.task)
+        taskDialog(task)
     }
 
     override fun onBoxChecked(task: TaskWithLabel) {
@@ -87,6 +94,11 @@ class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.Conf
         taskAdapter.resetDate()
         taskAdapter.notifyDataSetChanged()
         placeHolder()
+    }
+
+    private fun handleLabel(labels: List<Label>?) {
+        this.labels.clear()
+        this.labels.addAll(labels ?: listOf())
     }
 
     private fun deleteAll(){
@@ -115,40 +127,57 @@ class HistoryFragment : Fragment(), TaskAdapter.TaskCallback, GeneralHelper.Conf
         }
     }
 
-    private fun taskDialog(task: Task?) {
+    private fun taskDialog(task: TaskWithLabel?) {
         val dialog = Dialog(requireContext())
-        val binding: DialogTaskBinding = DialogTaskBinding.inflate(layoutInflater)
+        val binding = DialogTaskBinding.inflate(
+            layoutInflater
+        )
         dialog.setContentView(binding.root)
 
-        binding.delete.visibility = View.VISIBLE
-
-        // Views Setup
-        if (task != null) {
-            binding.title.setText(task.title)
-            binding.description.setText(task.description)
-            binding.delete.visibility = View.VISIBLE
+        // Hide Keyboard
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(dialog.window)
+                ?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
         }
+
+        // Chip Adapter
+        val chipAdapter = ChipAdapter(requireContext(), labels)
 
         // Match dialog window to screen width
         val window = dialog.window!!
-        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.WRAP_CONTENT)
+        window.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+
+        // Views Setup
+        if (task != null) {
+            chipAdapter.setSelectedLabels(task.labels)
+            binding.title.setText(task.task.title)
+            binding.description.setText(task.task.description)
+            binding.delete.visibility = View.VISIBLE
+        }
+        binding.labels.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.labels.adapter = chipAdapter
+
 
         // Listeners
-        binding.save.setOnClickListener{
-            task?.let {
-                task.title = binding.title.text.toString()
-                task.description = binding.description.text.toString()
-                taskViewModel.update(task)
+        binding.save.setOnClickListener {
+            task?.let{ task ->
+                task.task.title = Objects.requireNonNull(binding.title.text).toString()
+                task.task.description = Objects.requireNonNull(binding.description.text).toString()
+                task.task.labels = chipAdapter.getSelectedLabels()
+                taskViewModel.update(task.task)
             }
             dialog.dismiss()
         }
-
         binding.delete.setOnClickListener {
-            task?.let { taskViewModel.delete(it) }
+            if (task != null) {
+                taskViewModel.delete(task.task)
+            }
             dialog.dismiss()
         }
-
         dialog.show()
     }
 
